13097187f39675d183c22f39e39a9870995b0219
Ryu Takahashi <107231834+ryu-thakahashi@users.noreply.github.com>
2025-06-25 22:30:56 +0900

    add check_test question and validations

--- experiment_introduction/__init__.py
+++ experiment_introduction/__init__.py
@@ -20,8 +20,18 @@ class Group(BaseGroup):
     pass
 
 
+def set_payoff_form(form_label=""):
+    return models.IntegerField(
+        choices=[0, 6, 12, 18, 24, 4, 10, 16, 22, 28],
+        label=form_label,
+    )
+
+
 class Player(BasePlayer):
-    pass
+    offer_1 = set_payoff_form()
+    offer_2 = set_payoff_form()
+    offer_3 = set_payoff_form()
+    offer_4 = set_payoff_form()
 
 
 # PAGES
@@ -30,7 +40,17 @@ class Introduction(Page):
 
 
 class CheckTest(Page):
-    pass
+    form_model = "player"
+    form_fields = ["offer_1", "offer_2"]
+
+    @staticmethod
+    def error_message(player: Player, values):
+        all_correct = True
+        if values["offer_1"] != 0:
+            all_correct = False
+        if values["offer_2"] != 6:
+            all_correct = False
+        return "Please check your answers again." if not all_correct else None
 
 
 page_sequence = [Introduction, CheckTest]
