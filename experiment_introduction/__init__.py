18ba33b3d9434026adc51a2d8d46386e79ad71fe
Ryu Takahashi <107231834+ryu-thakahashi@users.noreply.github.com>
2025-06-27 22:09:20 +0900

    complete check test

--- experiment_introduction/__init__.py
+++ experiment_introduction/__init__.py
@@ -27,10 +27,25 @@ def set_payoff_form(form_label=""):
     )
 
 
+def set_question_form(form_label=""):
+    return models.IntegerField(
+        widget=widgets.RadioSelect,
+        choices=[[1, "正しい"], [0, "誤り"]],
+        label=form_label,
+    )
+
+
 class Player(BasePlayer):
-    question_1 = set_payoff_form()
-    question_2 = set_payoff_form()
-    question_3 = set_payoff_form()
+    q_1 = set_payoff_form()
+    q_2 = set_payoff_form()
+    q_3 = set_payoff_form()
+    q_4_1 = set_question_form(
+        "やりとりを行うたびに、あなたがやりとりをする 4人の相手は変わる"
+    )
+    q_4_2 = set_question_form(
+        "あなたが獲得するポイントは、あなたの選択とつながりのある4人の相手の選択の組み合わせによって決まる"
+    )
+    q_4_3 = set_question_form("課題の繰り返し回数は、事前にはみなさんには知らされない")
 
 
 # PAGES
@@ -40,17 +55,25 @@ class Introduction(Page):
 
 class CheckTest(Page):
     form_model = "player"
-    form_fields = ["question_1", "question_2", "question_3"]
+    form_fields = ["q_1", "q_2", "q_3", "q_4_1", "q_4_2", "q_4_3"]
 
     @staticmethod
     def error_message(player: Player, values):
+        correct_answers = {
+            "q_1": 22,
+            "q_2": 4,
+            "q_3": 6,
+            "q_4_1": 0,
+            "q_4_2": 1,
+            "q_4_3": 1,
+        }
+
         all_correct = True
-        if values["question_1"] != 22:
-            all_correct = False
-        if values["question_2"] != 4:
-            all_correct = False
-        if values["question_3"] != 6:
-            all_correct = False
+        for question, answer in correct_answers.items():
+            if values[question] != answer:
+                all_correct = False
+                break
+
         return "誤りがあります．再度ご回答ください．" if not all_correct else None
 
 
